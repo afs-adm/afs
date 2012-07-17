8a74eb79edb23f31f4d4ed1eb34ad32e34ea9f8c
mitya <mitya_msu@mail.ru>
2012-07-17 17:06:27 +0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afs::Application.config.secret_token = '6e0c0e1c065b8f5e1c74bc624dc83d35ec32366a1157810bbb1277ee12fd8a28744191b0a1d38034928fc8b80c68d330e2bdcebd507cb825495909b9733bc909'
