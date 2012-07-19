0784bd237d7efc607ae648e9f91d9ea7370da8c9
afs-adm <afs-adm@yandex.ru>
2012-07-19 17:46:08 +0600

    some fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Afs::Application.routes.draw do
   
+  get "challengers/index"
+
+  get "challengers/create"
+
+  get "comments/index"
+
+  get "comments/create"
+
+  get "comments/activate"
+
 end
