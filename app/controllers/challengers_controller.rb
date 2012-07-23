d816e625c10f18a2d99482daf5359c10994149e4
afs-adm <afs-adm@yandex.ru>
2012-07-23 16:31:38 +0600

    fixes

--- app/controllers/challengers_controller.rb
+++ app/controllers/challengers_controller.rb
@@ -1,7 +1,10 @@
+#coding: utf-8
 class ChallengersController < ApplicationController
   def index
   end
 
   def create
+    flash[:notice] = "Ваша заявка принята"
+    redirect_to :index
   end
 end
